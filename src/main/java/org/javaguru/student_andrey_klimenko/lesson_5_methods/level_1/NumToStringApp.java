22f4eb175fdda21864b4eae0ceeb4eed057b9e1b
Andy-planchet <balans8bak@mail.ru>
2025-07-16 09:11:45 +0300

    16-07-25

---
+++ src/main/java/org/javaguru/student_andrey_klimenko/lesson_5_methods/level_1/NumToStringApp.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_andrey_klimenko.lesson_5_methods.level_1;
+
+class NumToStringApp {
+    public static void main(String[] args) {
+        NumToString numToString=new NumToString();
+
+        System.out.println("Num 5 "+numToString.str(5));
+        System.out.println("Num 15 "+numToString.str(15));
+        System.out.println("Num 10 "+numToString.str(10));
+    }
+}
